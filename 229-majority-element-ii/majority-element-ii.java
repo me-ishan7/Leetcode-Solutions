18b94fe398e2678f5fa6bcd943bb931a10f37ae0
me-ishan7 <ik1362005@gmail.com>
2025-10-28 23:32:50 +0530

    Time: 2 ms (99.78%) | Memory: 50.9 MB (5.05%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.java
+++ 229-majority-element-ii/majority-element-ii.java
@@ -1,15 +1,38 @@
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
-        List<Integer> list = new ArrayList<>();
-        HashMap<Integer,Integer> map = new HashMap<>();
-        int size = nums.length;
-        int mini = (int)(size / 3) + 1;
+        int count1 = 0, count2 = 0;
+        int candidate1 = Integer.MIN_VALUE;
+        int candidate2 = Integer.MIN_VALUE;
+        int n = nums.length;
+       
         for(int num : nums) {
-            map.put(num , map.getOrDefault(num , 0) + 1);
-            if(map.get(num) == mini){
-                list.add(num);
+            if(count1 == 0 && num != candidate2){
+                candidate1 = num;
+                count1 = 1;
             }
+            else if(count2 == 0 && num != candidate1) {
+                candidate2 = num;
+                count2 = 1;
+            }
+            else if(num == candidate1) count1++;
+            else if(num == candidate2) count2++;
+            else{
+                count1--;
+                count2--;
+            } 
+            
         }
+        List<Integer> list = new ArrayList<>();
+        count1 = 0;
+        count2 = 0;
+        for(int num : nums) {
+            if(candidate1 == num) count1++;
+            if(candidate2 == num) count2++;
+        }
+        int mini = n / 3;
+        if(count1 > mini) list.add(candidate1);
+        if(count2 > mini) list.add(candidate2);
+        
         return list;
     }
 }
