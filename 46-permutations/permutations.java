d6df318fd83146f0c358a3e3544f6bdd99c01136
me-ishan7 <ik1362005@gmail.com>
2026-04-17 11:58:47 +0530

    Time: 1 ms (86.10%) | Memory: 45.8 MB (12.68%) - LeetSync

--- 46-permutations/permutations.java
+++ 46-permutations/permutations.java
@@ -2,28 +2,32 @@ class Solution {
     public List<List<Integer>> permute(int[] nums) {
         int n = nums.length;
         List<List<Integer>> ans = new ArrayList<>();
-        ArrayList<Integer> list = new ArrayList<>();
-        boolean[] freq = new boolean[n];
-
-        findPermutations(nums, 0, n, list, ans);
+        findPermutations(nums, 0, n, ans);
         return ans;
     }
 
     private static void findPermutations(int[] nums, int curr, int n, 
-    ArrayList<Integer> list, List<List<Integer>> ans) {
+    List<List<Integer>> ans) {
 
-        if(list.size() == n) {
-            ans.add(new ArrayList<Integer>(list));
+        if(curr == n) {
+            ArrayList<Integer> list = new ArrayList<>();
+            for(int i = 0; i < n; i++) {
+                list.add(nums[i]);
+            }
+            ans.add(new ArrayList<>(list));
             return;
         }
     
-        for(int i = 0; i < n; i++) {
-            if(!list.contains(nums[i])) {
-                list.add(nums[i]); 
-                findPermutations(nums, i +1, n, list, ans);
-                list.remove(list.size() - 1);
-            }
-            
+        for(int i = curr; i < n; i++) {
+            swap(nums, curr, i);
+            findPermutations(nums, curr +1, n, ans);
+            swap(nums, curr, i);
         }   
+    }
+
+    static void swap(int[] arr, int a, int b) {
+        int temp = arr[a];
+        arr[a] = arr[b];
+        arr[b] = temp;
     }
 }
