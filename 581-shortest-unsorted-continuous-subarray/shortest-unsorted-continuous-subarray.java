56c10935143031c8d19ae632d366c40f3e81184f
me-ishan7 <ik1362005@gmail.com>
2026-06-23 13:10:12 +0530

    Time: 1 ms (89.90%) | Memory: 47.9 MB (21.34%) - LeetSync

--- 581-shortest-unsorted-continuous-subarray/shortest-unsorted-continuous-subarray.java
+++ 581-shortest-unsorted-continuous-subarray/shortest-unsorted-continuous-subarray.java
@@ -2,26 +2,39 @@ class Solution {
     public int findUnsortedSubarray(int[] nums) {
         int n = nums.length;
         int[] arr = new int[n];
+
+        if(n == 1) return 0;
         
-        for(int i = 0; i < n; i++) {
-            arr[i] = nums[i];
-        }
-
-        Arrays.sort(nums);
-
-        int start = -1, end = -1;
+        int min = Integer.MAX_VALUE;
+        int max = Integer.MIN_VALUE;
 
         for(int i = 0; i < n; i++) {
-            if(arr[i] != nums[i] && start == -1){
-                start = i;
-                end = i;
+            if(i == 0) {
+                if(nums[i] > nums[i + 1]){
+                    min = Math.min(min, nums[i]);
+                    max = Math.max(max, nums[i]);
+                }
             }
-            if(arr[i] != nums[i]){
-                end = i;
+
+            else if(i == n - 1){
+                if(nums[i] < nums[i - 1]){
+                    min = Math.min(min, nums[i]);
+                    max = Math.max(max, nums[i]);
+                }
+            }
+
+            else if((nums[i] > nums[i + 1]) || (nums[i] < nums[i - 1])){
+                min = Math.min(min, nums[i]);
+                max = Math.max(max, nums[i]);
             }
 
         }
-        if(start == -1) return 0;
+        if(min == Integer.MAX_VALUE || max == Integer.MIN_VALUE) return 0;
+        int start = 0, end = 0;
+
+        for(start = 0; start < n && nums[start] <= min; start++);
+        for(end = n - 1; end >= 0 && nums[end] >= max; end--);
+
         return end - start + 1;
     }
 }
