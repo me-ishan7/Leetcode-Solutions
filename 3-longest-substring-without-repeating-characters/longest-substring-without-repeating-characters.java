b2659244db3914a90a826365f996b128bb0c5c67
me-ishan7 <ik1362005@gmail.com>
2026-06-23 23:58:25 +0530

    Time: 6 ms (69.75%) | Memory: 46.5 MB (38.15%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.java
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.java
@@ -3,9 +3,11 @@ class Solution {
         int n = s.length();
         int left = 0;
         int longestSubstr = 0;
-        HashMap<Character, Integer> map = new HashMap<>();
+        //HashMap<Character, Integer> map = new HashMap<>();
+        HashSet<Character> set = new HashSet<>();
 
         for(int right = 0; right < n; right++) {
+            /*
             //Include right
             map.put(s.charAt(right), map.getOrDefault(s.charAt(right), 0) + 1);
 
@@ -16,7 +18,21 @@ class Solution {
             }
 
             longestSubstr = Math.max(longestSubstr, right - left + 1);
+            */
+
+            //In HashSet we have Process left first because it will remove element on the process left step if we use this just after Include right at each element addition to the hashset.
+
+            //Process left
+            while(set.contains(s.charAt(right))){
+                set.remove(s.charAt(left));
+                left++;
+            }
+
+            //Include right
+            set.add(s.charAt(right));
             
+            //store result
+            longestSubstr = Math.max(longestSubstr, right - left + 1);
         }
         return longestSubstr;
     }
