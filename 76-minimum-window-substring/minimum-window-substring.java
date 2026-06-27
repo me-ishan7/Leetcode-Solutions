d0933482436ccfd6577e8558efe369a7b20ff52d
me-ishan7 <ik1362005@gmail.com>
2026-06-27 15:22:04 +0530

    Time: 118 ms (11.26%) | Memory: 47.7 MB (25.11%) - LeetSync

---
+++ 76-minimum-window-substring/minimum-window-substring.java
@@ -0,0 +1,53 @@
+class Solution {
+    public String minWindow(String s, String t) {
+
+        if(s.length() < t.length())
+            return "";
+
+        //store t in hashmap      
+        HashMap<Character, Integer> map = new HashMap<>();
+
+        for (char c : t.toCharArray()) {
+            map.put(c, map.getOrDefault(c, 0) + 1);
+        }
+
+        int left = 0;
+        int required = t.length();
+
+        int minLen = Integer.MAX_VALUE;
+        String res = "";
+
+        for (int right = 0; right < s.length(); right++) {
+            //Include Right
+            char ch = s.charAt(right);
+            if (map.containsKey(ch)) {
+
+                // decrement required only when value of ch > 0(prevents to decrement when duplicates occurs)
+                if (map.get(ch) > 0)
+                    required--;
+
+                map.put(ch, map.get(ch) - 1);
+            }
+
+            // if window is valid precess left but first store answer bcz - minWindow question;
+            while (required == 0) {
+                if(right - left + 1 < minLen) {
+                    minLen = right - left + 1;
+                    res = s.substring(left, right + 1);
+                }
+
+                char leftChar = s.charAt(left);
+
+                if(map.containsKey(leftChar)) {
+
+                    map.put(leftChar, map.get(leftChar) + 1);
+
+                    if(map.get(leftChar) > 0)
+                        required++;
+                }
+                left++;
+            }
+        }
+        return minLen == Integer.MAX_VALUE ? "" : res;
+    }
+}
