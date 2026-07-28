eaffed5185c6be0a56d78d568a320cbec2e7532d
me-ishan7 <ik1362005@gmail.com>
2026-07-28 16:57:54 +0530

    Time: 11 ms (90.42%) | Memory: 47.5 MB (20.20%) - LeetSync

--- 1128-remove-all-adjacent-duplicates-in-string/remove-all-adjacent-duplicates-in-string.java
+++ 1128-remove-all-adjacent-duplicates-in-string/remove-all-adjacent-duplicates-in-string.java
@@ -1,24 +1,39 @@
 class Solution {
     public String removeDuplicates(String s) {
-        Deque<Character> st = new ArrayDeque<>();
+        // Deque<Character> st = new ArrayDeque<>();
 
-        int i = 0, n = s.length();
+        // int i = 0, n = s.length();
 
-        while(i < n){
-            if(!st.isEmpty() && s.charAt(i) == st.peek()){
-                st.pop();
-            }
-            else {
-                st.push(s.charAt(i));
-            }
-            i++;
-        }
+        // while(i < n){
+        //     if(!st.isEmpty() && s.charAt(i) == st.peek()){
+        //         st.pop();
+        //     }
+        //     else {
+        //         st.push(s.charAt(i));
+        //     }
+        //     i++;
+        // }
+
+        // StringBuilder sb = new StringBuilder();
+
+        // while(!st.isEmpty()){
+        //     sb.append(st.pop());
+        // }
+        // return sb.reverse().toString();
 
         StringBuilder sb = new StringBuilder();
+        int n = s.length();
 
-        while(!st.isEmpty()){
-            sb.append(st.pop());
+        for(char ch : s.toCharArray()) {
+            int len = sb.length();
+
+            if(len > 0 && ch == sb.charAt(len - 1)){
+                sb.deleteCharAt(len - 1);
+            }
+            else{
+                sb.append(ch);
+            }
         }
-        return sb.reverse().toString();
+        return sb.toString();
     }
 }
