aa3892458544becc3d6194a38f51c18f8eae2de0
me-ishan7 <ik1362005@gmail.com>
2026-07-29 14:12:52 +0530

    Time: 6 ms (90.34%) | Memory: 48.3 MB (75.53%) - LeetSync

---
+++ 503-next-greater-element-ii/next-greater-element-ii.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int[] nextGreaterElements(int[] nums) {
+        int n = nums.length;
+        int[] ans = new int[n];
+
+        Deque<Integer> st = new ArrayDeque<>();
+
+        for(int i = 2 * n - 1; i >= 0; i--) {
+            while(!st.isEmpty() && st.peek() <= nums[i % n]){
+                st.pop();
+            }
+            if(st.isEmpty()) {
+                ans[i % n] = -1;
+            }
+            else{
+                ans[i % n] = st.peek();
+            }
+            st.push(nums[i % n]);
+        }
+        return ans;
+    }
+}
