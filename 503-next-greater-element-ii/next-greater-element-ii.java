0fd8f6d7843c0c064651a5d85b253feec55a6586
me-ishan7 <ik1362005@gmail.com>
2026-07-29 17:28:59 +0530

    Time: 5 ms (95.06%) | Memory: 48.8 MB (13.07%) - LeetSync

--- 503-next-greater-element-ii/next-greater-element-ii.java
+++ 503-next-greater-element-ii/next-greater-element-ii.java
@@ -5,17 +5,34 @@ class Solution {
 
         Deque<Integer> st = new ArrayDeque<>();
 
-        for(int i = 2 * n - 1; i >= 0; i--) {
-            while(!st.isEmpty() && st.peek() <= nums[i % n]){
+        // for(int i = 2 * n - 1; i >= 0; i--) {
+        //     while(!st.isEmpty() && st.peek() <= nums[i % n]){
+        //         st.pop();
+        //     }
+        //     if(st.isEmpty()) {
+        //         ans[i % n] = -1;
+        //     }
+        //     else{
+        //         ans[i % n] = st.peek();
+        //     }
+        //     st.push(nums[i % n]);
+        // }
+        // return ans;
+
+        //har ek element ko equal chance milna chahie to stack me pehle hi sare elements store kara lo
+        for(int i = n - 2; i >= 0; i--) {
+            st.push(nums[i]);
+        }
+
+        // ab simple next greater element ka code likh do
+
+        for(int i = n - 1; i >= 0; i--) {
+            while(!st.isEmpty() && st.peek() <= nums[i]){
                 st.pop();
             }
-            if(st.isEmpty()) {
-                ans[i % n] = -1;
-            }
-            else{
-                ans[i % n] = st.peek();
-            }
-            st.push(nums[i % n]);
+            if(st.isEmpty()) ans[i] = -1;
+            else ans[i] = st.peek();
+            st.push(nums[i]);
         }
         return ans;
     }
